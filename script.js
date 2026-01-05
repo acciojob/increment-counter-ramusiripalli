ff0153856456594547e3b74f1bc75370df3f6004
ramu siripalli <ramsiripalli2772@gmail.com>
2026-01-05 17:25:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+
+function counter(){
+	const counter = document.getElementById("counter");
+	counter.innerHTML = Number(counter.innerHTML) + 1;
+}
+
+
+const btn = document.getElementById("incrementBtn");
+btn.addEventListener("click",counter);
+
+
+

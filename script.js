b706cf96a30bff694911353d8ce10f9efddcf324
ramu siripalli <ramsiripalli2772@gmail.com>
2026-06-29 12:45:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+
+function increment(){
+	let counter = document.getElementById("counter");
+	counter.textContent = parseInt(counter.innerHTML)+1;
+}
+
+const btn = document.getElementById("incrementBtn");
+btn.addEventListener("click",increment)
